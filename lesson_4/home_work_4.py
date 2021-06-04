be9600b2ad2cba64443bb8389e5d8076bd1fcf71
Daria-Mimi <dariamimi@yandex.ru>
2021-06-04 21:15:30 +0300

    lesson_4

---
+++ lesson_4/home_work_4.py
@@ -0,0 +1,36 @@
+# заменить каждый 3-й элемент на *
+# some_string = input("Введите предложение: ")
+# res = " "
+# print(len(some_string))
+# cnt = 0
+# while cnt < len(some_string):
+#     if cnt % 3 == 0:
+#         res += "*"
+#     else:
+#         res += some_string [cnt]
+#     cnt +=1
+# print(res)
+
+# заменить каждый четный символ на А
+# some_string = input('Введите предложение: ')
+# cnt = 0
+# res = ''
+# while cnt < len(some_string):
+#     if cnt % 2 == 0:
+#         res += "B"
+#     else:
+#         res += some_string[cnt]
+#     cnt += 1
+# print(res)
+
+# заменить каждый 5 символ строки
+# some_string = input("Введите предложение: ")
+# cnt = 0
+# res = ""
+# while cnt < len(some_string):
+#     if cnt % 5 == 0:
+#         res += "^"
+#     else:
+#         res += some_string[cnt]
+#     cnt += 1
+# print(res)
