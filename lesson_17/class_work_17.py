04aa1d34eaff1fd6dbd5e3d6adfd0067c4ed1712
Daria-Mimi <dariamimi@yandex.ru>
2021-07-10 20:25:05 +0300

    class_work_17

--- lesson_17/class_work_17.py
+++ lesson_17/class_work_17.py
@@ -88,93 +88,93 @@ class Elephant:
             self.is_moving = False
 
 
-# elephant = Elephant('grey', 15, 25, 'Trevor')
-# elephant_2 = Elephant('grey', 15, 25, 'German')
-# print(Elephant)
-# print(elephant)
-# elephant.go()
-# elephant_2.go()
-# elephant_2.go()
-# elephant_2.go()
-# elephant_2.go()
-# elephant_2.stop()
-# elephant_2.stop()
-# elephant_2.stop()
-# elephant_2.stop()
-# elephant_2.go()
-# elephant_2.stop()
-# elephant_2.go()
-# elephant_2.stop()
+elephant = Elephant('grey', 15, 25, 'Trevor')
+elephant_2 = Elephant('grey', 15, 25, 'German')
+print(Elephant)
+print(elephant)
+elephant.go()
+elephant_2.go()
+elephant_2.go()
+elephant_2.go()
+elephant_2.go()
+elephant_2.stop()
+elephant_2.stop()
+elephant_2.stop()
+elephant_2.stop()
+elephant_2.go()
+elephant_2.stop()
+elephant_2.go()
+elephant_2.stop()
 # print(1, elephant.is_moving)
 # print(2, elephant_2.is_moving)
 
-from uuid import uuid4
-from datetime import datetime
-
-
-# задача: сделать приложение для продажи театральных билетов и их учета.
-class Ticket:
-    def __init__(self, event_date, event_address, event_name, price):
-        self.event_date = event_date
-        self.event_address = event_address
-        self.event_name = event_name
-        self.price = price
-        self.unique_id = uuid4()  # сгенерит уникальный рандомный идентификатор
-        self.is_active = True
-
-
-class TicketSeller:
-    """Продаватель билетов"""
-    def sell_ticket(self, *args, **kwargs):
-        ticket = Ticket(*args, **kwargs)
-        self.notify_head_office(ticket)
-        self.notify_client(ticket)
-        self.write_data_to_database(ticket)
-        return ticket
-
-    def notify_head_office(self, ticket):
-        print(f"TO HEAD OFFICE: Ticket with id {ticket.unique_id} has been sold at {datetime.now()}")
-
-    def notify_client(self, ticket):
-        print(f"TO CLIENT: Ticket with id {ticket.unique_id} has been sold at {datetime.now()}")
-
-    def write_data_to_database(self, ticket):
-        print(f"TO DATABASE: Ticket with id {ticket.unique_id} has been sold at {datetime.now()}")
-
-
-class TicketUsabler:
-    def use_ticket(self, ticket):
-        ticket.is_active = False
-        self.notify_database(ticket)
-
-    def notify_database(self, ticket):
-        print(f"TO DATABASE: {ticket.unique_id} has been used!")
-
-
-ticket_data = [
-    {
-        "event_date": "2021-07-07 20:00:00",
-        "event_address": "Moscow",
-        "event_name": "Kish Concert",
-        "price": 100000
-    },
-    {
-        "event_date": "2021-11-07 19:00:00",
-        "event_address": "Omsk",
-        "event_name": "Kipelov",
-        "price": 200000
-    },
-    {
-        "event_date": "2021-12-07 19:00:00",
-        "event_address": "St.Petersburg",
-        "event_name": "Ariya",
-        "price": 300000
-    },
-]
-
-seller = TicketSeller()  # создал продавателя билетов
-usabler = TicketUsabler()
-l = [seller.sell_ticket(**ticket_params) for ticket_params in ticket_data]
-for ticket in l:
-    usabler.use_ticket(ticket)
-c = 1
+# from uuid import uuid4
+# from datetime import datetime
+#
+#
+# # задача: сделать приложение для продажи театральных билетов и их учета.
+# class Ticket:
+#     def __init__(self, event_date, event_address, event_name, price):
+#         self.event_date = event_date
+#         self.event_address = event_address
+#         self.event_name = event_name
+#         self.price = price
+#         self.unique_id = uuid4()  # сгенерит уникальный рандомный идентификатор
+#         self.is_active = True
+#
+#
+# class TicketSeller:
+#     """Продаватель билетов"""
+#     def sell_ticket(self, *args, **kwargs):
+#         ticket = Ticket(*args, **kwargs)
+#         self.notify_head_office(ticket)
+#         self.notify_client(ticket)
+#         self.write_data_to_database(ticket)
+#         return ticket
+#
+#     def notify_head_office(self, ticket):
+#         print(f"TO HEAD OFFICE: Ticket with id {ticket.unique_id} has been sold at {datetime.now()}")
+#
+#     def notify_client(self, ticket):
+#         print(f"TO CLIENT: Ticket with id {ticket.unique_id} has been sold at {datetime.now()}")
+#
+#     def write_data_to_database(self, ticket):
+#         print(f"TO DATABASE: Ticket with id {ticket.unique_id} has been sold at {datetime.now()}")
+#
+#
+# class TicketUsabler:
+#     def use_ticket(self, ticket):
+#         ticket.is_active = False
+#         self.notify_database(ticket)
+#
+#     def notify_database(self, ticket):
+#         print(f"TO DATABASE: {ticket.unique_id} has been used!")
+#
+#
+# ticket_data = [
+#     {
+#         "event_date": "2021-07-07 20:00:00",
+#         "event_address": "Moscow",
+#         "event_name": "Kish Concert",
+#         "price": 100000
+#     },
+#     {
+#         "event_date": "2021-11-07 19:00:00",
+#         "event_address": "Omsk",
+#         "event_name": "Kipelov",
+#         "price": 200000
+#     },
+#     {
+#         "event_date": "2021-12-07 19:00:00",
+#         "event_address": "St.Petersburg",
+#         "event_name": "Ariya",
+#         "price": 300000
+#     },
+# ]
+#
+# seller = TicketSeller()  # создал продавателя билетов
+# usabler = TicketUsabler()
+# l = [seller.sell_ticket(**ticket_params) for ticket_params in ticket_data]
+# for ticket in l:
+#     usabler.use_ticket(ticket)
+# c = 1
