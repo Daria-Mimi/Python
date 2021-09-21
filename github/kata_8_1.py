6f0f63b5936db9b2d3112dab90cdfdd33c88dc9d
Daria-Mimi <dariamimi@yandex.ru>
2021-09-21 17:00:48 +0300

    github

---
+++ github/kata_8_1.py
@@ -0,0 +1,36 @@
+"""
+https://www.codewars.com/kata/570e8ec4127ad143660001fd
+https://www.codewars.com/kata/57a1fd2ce298a731b20006a4
+https://www.codewars.com/kata/55ecd718f46fba02e5000029
+
+"""
+# 1
+# def billboard(name, price=30):
+#     return sum(price for el in name)
+#
+# def billboard(name, price = 30):
+#     return len(name) * price
+#
+# print(billboard('Daria Mi', 30))
+
+# 2
+# def is_palindrome(s):
+#     s = s.lower()
+#     if s == s[::-1]:
+#         return True
+#     else:
+#         return False
+
+# def is_palindrome(s):
+#     s = s.lower()
+#     return s == s[::-1]
+
+# def is_palindrome(s):
+#     return s.lower() == s.lower()[::-1]
+
+# print(is_palindrome("топот"))
+# print(is_palindrome("Дарья"))
+
+# 3
+# def between(a,b):
+#
