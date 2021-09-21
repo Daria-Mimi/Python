26f2beb73b70abbe86c88f8f076900ec8d2d0e80
Daria-Mimi <dariamimi@yandex.ru>
2021-09-21 17:01:15 +0300

    hm26

--- lesson_26/home_work_26.py
+++ lesson_26/home_work_26.py
@@ -3,6 +3,11 @@ import telebot
 bot = telebot.TeleBot('1944569305:AAHtdGr1PKk5QHBK-fow25aJEgMxC7MUHZ4')
 
 # TOKEN = "1944569305:AAHtdGr1PKk5QHBK-fow25aJEgMxC7MUHZ4"
+#
+# @bot.message_handler(content_types=['Hello!'])
+# def get_text_messages(message):
 
-@bot.message_handler(content_types=['Hello!'])
-def get_text_messages(message):
+
+token = '123456'
+TelegramBot = telebot.Bot(token)
+print(TelegramBot.getMe())
