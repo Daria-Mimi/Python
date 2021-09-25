e835290bf7b7209037009a552e826c59cae4d248
Daria-Mimi <dariamimi@yandex.ru>
2021-09-25 18:37:17 +0300

    hw35

---
+++ lesson_35/home_work_35.py
@@ -0,0 +1,12 @@
+"""
+create table users (
+name text,
+surname text,
+age int,
+city text
+);
+
+insert into users values ('Daria', 'Mishankova', 28, 'Moscow');
+insert into users values ('Anna', 'Mishankova', 23, 'Moscow');
+select * from users;
+"""
