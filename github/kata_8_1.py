b09640a10bab9b9bcc81d422a732d2d80b0e1dd9
Daria-Mimi <dariamimi@yandex.ru>
2021-09-25 17:22:44 +0300

    gh

--- github/kata_8_1.py
+++ github/kata_8_1.py
@@ -2,6 +2,10 @@
 https://www.codewars.com/kata/570e8ec4127ad143660001fd
 https://www.codewars.com/kata/57a1fd2ce298a731b20006a4
 https://www.codewars.com/kata/55ecd718f46fba02e5000029
+https://www.codewars.com/kata/50ee6b0bdeab583673000025
+https://www.codewars.com/kata/55fab1ffda3e2e44f00000c6
+
+
 
 """
 # 1
@@ -32,6 +36,20 @@ https://www.codewars.com/kata/55ecd718f46fba02e5000029
 # print(is_palindrome("Дарья"))
 
 # 3
-# def between(a,b):
+# def between(a, b):
+#     return list(range(a, b + 1))
 #
+# print(between(1, 5))
+
+# 4
+# a = "code"
+# b = "wa.rs"
+# name = a + b
+#
+# print(name)
+
+# 5
+def cockroach_speed(s):
+    
+
 
