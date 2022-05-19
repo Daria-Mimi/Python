51c9e376695b6c3f011c5deb19fcdcd85407a4f5
Daria-Mimi <daria.mimi.212@gmail.com>
2022-05-19 17:40:24 +0300

    hw_oop

--- lesson_luchanos/task_oop.py
+++ lesson_luchanos/task_oop.py
@@ -8,10 +8,10 @@ class Point:
         self.__coord_x = x
         self.__coord_y = y
 
-    def get_coord(self):
+    def __str__(self):
         return f"x coord: {self.__coord_x}, y coord: {self.__coord_y}"
 
 
 point = Point()
 point.coord(3, 5)
-print(point.get_coord())
+print(point)
