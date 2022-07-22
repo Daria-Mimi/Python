7fac80acbbf38e95f0179dcf368b2f4715a1dbc6
Daria-Mimi <daria.mimi.212@gmail.com>
2022-07-22 13:09:38 +0300

    bot

---
+++ tasks/tasks_6.py
@@ -0,0 +1,49 @@
+"""
+https://www.codewars.com/kata/53da6d8d112bd1a0dc00008b
+https://www.codewars.com/kata/58261acb22be6e2ed800003a
+https://www.codewars.com/kata/568dcc3c7f12767a62000038
+https://www.codewars.com/kata/567bf4f7ee34510f69000032
+
+
+"""
+# 1
+# def reverse_list(l):
+#     return l[::-1]
+#
+# print(reverse_list([1,2,3,4]))
+
+
+# 2
+# def get_volume_of_cuboid(length, width, height):
+#     return length * width * height
+#
+# print(get_volume_of_cuboid(1, 2, 2)) #4
+# print(get_volume_of_cuboid(6.3, 2, 5)) #63
+
+
+# 3
+# def set_alarm(employed, vacation):
+#     if employed == True and vacation == False:
+#         return True
+#     else:
+#         return False
+#
+# print(set_alarm(True, True))
+# print(set_alarm(False, True))
+# print(set_alarm(False, False))
+# print(set_alarm(True, False))
+#
+# def set_alarm(employed, vacation):
+#     return employed and not vacation
+
+
+# 4
+# def is_digit(n):
+#     return n.isdigit() and len(n) == 1
+#
+# print(is_digit("7")) # True
+# print(is_digit(" ")) # False
+# print(is_digit("a")) #False
+# print(is_digit("a5")) # False
+
+
