16ba1bcc1f342767ff71d259abb1d5d54db23616
Daria-Mimi <daria.mimi.212@gmail.com>
2022-07-27 21:47:48 +0300

    fastAPI 2

--- fastAPI/main.py
+++ fastAPI/main.py
@@ -1,9 +1,46 @@
-from fastapi import FastAPI
+from typing import List
 
+from fastapi import FastAPI, Query, Path, Body
+from schemas import Book, Author, BookOut
 
 app = FastAPI()
 
 
-@app.get('/')
-def home():
-    return {"key": "Hello"}
+@app.post('/book', response_model=BookOut)
+def create_book(item: Book):
+    return BookOut(**item.dict(), id=3)
+
+
+# @app.get('/')
+# def home():
+#     return {"key": "Hello"}
+#
+#
+# @app.get('/{pk}')
+# def get_item(pk: int, q: int = None):
+#     return {"key": pk, "q": q}
+#
+#
+# @app.get('/user/{pk}/items/{item}')
+# def get_user_item(pk: int, item: str):
+#     return {"user": pk, "item": item}
+
+
+# @app.post('/book')
+# def create_book(item: Book, author: Author, quantity: int = Body(...)):
+#     return {"item": item, "author": author, "quantity": quantity}
+
+
+# @app.post('/author')
+# def create_author(author: Author = Body(..., embed=True)):
+#     return {"author": author}
+#
+#
+# @app.get('/book')
+# def get_book(q: List[str] = Query(["test", "test2"], description="Search book")):
+#     return q
+#
+#
+# @app.get('/book/{pk}')
+# def get_single_book(pk: int = Path(..., gt=1, le=20), pages: int = Query(None, gt=10, le= 500)):
+#     return {"pk": pk, "pages": pages}
